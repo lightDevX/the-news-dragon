5863ef522e19b9fc8dc261618ee92f7907c72cf0
xcoderkhan <khan.m.hasanuzzaman@gmail.com>
2023-07-11 01:49:17 +0600

    U

--- src/layouts/Shared/RightNav/RightNav.jsx
+++ src/layouts/Shared/RightNav/RightNav.jsx
@@ -1,11 +1,12 @@
 import { Button } from '@material-tailwind/react';
 import React from 'react';
+import { FaBeer } from 'react-icons/fa';
 
 const RightNav = () => {
     return (
         <div>
             <h3>Login</h3>
-            <Button>Login with Google</Button>
+            <Button><FaBeer/>Login with Google</Button>
             <Button>Login with GitHub</Button>
         </div>
     );
