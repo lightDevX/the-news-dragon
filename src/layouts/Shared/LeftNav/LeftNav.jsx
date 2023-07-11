fc08681734d210e86704890bf577a05d735a7330
xcoderkhan <khan.m.hasanuzzaman@gmail.com>
2023-07-11 21:30:51 +0600

    Left nav update

--- src/layouts/Shared/LeftNav/LeftNav.jsx
+++ src/layouts/Shared/LeftNav/LeftNav.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+            .catch(error => console.error(error))
+
+    }, [])
+
     return (
         <div>
-            <h3 className="text-center">Left nav</h3>
+            <h3 className="text-center">All Category:</h3>
+            <div className=' ps-8 text-white space-y-1'>
+                {
+                    categories.map(category => <p
+                        key={category.id}
+                    >
+                        <Link to={`/category/${category.id}`}>{category.name}</Link>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
