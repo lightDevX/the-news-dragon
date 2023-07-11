c31678e12289e6d1e630e6811133788d377e351e
MD HASANUZZAMAN KHAN <khan.m.hasanuzzaman@gmail.com>
2023-07-11 13:56:13 +0600

    eslint

--- src/layouts/Shared/RightNav/RightNav.jsx
+++ src/layouts/Shared/RightNav/RightNav.jsx
@@ -1,13 +1,18 @@
 import { Button } from '@material-tailwind/react';
 import React from 'react';
-import { FaBeer } from 'react-icons/fa';
+import { FaGoogle, } from 'react-icons/fa';
 
 const RightNav = () => {
     return (
         <div>
             <h3>Login</h3>
-            <Button><FaBeer/>Login with Google</Button>
-            <Button>Login with GitHub</Button>
+            <div className=' flex flex-col gap-2 p-5'>
+                <Button className=' w-60 h-10 flex items-center gap-1 bg-red-200'><FaGoogle className=' text-blue-600'/> Login with Google</Button>
+                <Button className=' w-60 h-10'>Login with GitHub</Button>
+            </div>
+            <div>
+                <h3>Find us on:</h3>
+            </div>
         </div>
     );
 };
